e266e83dd44b4fb752933dc0aed1cc54edc582e0
AlexandrBxbrxv <g1026@bk.ru>
2024-10-31 19:57:38 +0500

    + модель Analysis

--- services/models.py
+++ services/models.py
@@ -1,1 +1,19 @@
 from django.db import models
+
+
+class Analysis(models.Model):
+    """Модель анализа"""
+    title = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preparation = models.TextField(verbose_name='подготовка')
+
+    treatment_room = models.PositiveSmallIntegerField(verbose_name='процедурный кабинет')
+    price = models.PositiveSmallIntegerField(verbose_name='цена')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'анализ'
+        verbose_name_plural = 'анализы'
+        ordering = ('price',)
