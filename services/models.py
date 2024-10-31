e75381fa838cfac081ac8b0c5dc8007d8c67b19b
AlexandrBxbrxv <g1026@bk.ru>
2024-10-31 20:58:24 +0500

    + модель Appointment

--- services/models.py
+++ services/models.py
@@ -20,6 +20,26 @@ class Doctor(models.Model):
         verbose_name_plural = 'врачи'
 
 
+class Appointment(models.Model):
+    """Модель записи на приём ко врачу."""
+    title = models.CharField(max_length=200, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, **NULLABLE, related_name='appointments_doctor',
+                               verbose_name='врач')
+
+    treatment_room = models.PositiveSmallIntegerField(verbose_name='процедурный кабинет')
+    price = models.PositiveSmallIntegerField(verbose_name='цена')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'прием'
+        verbose_name_plural = 'приемы'
+        ordering = ('price',)
+
+
 class Analysis(models.Model):
     """Модель анализа."""
     title = models.CharField(max_length=200, verbose_name='название')
