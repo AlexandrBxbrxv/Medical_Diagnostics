0cd133a7a994cdf08287d41b912466af96c7c694
AlexandrBxbrxv <g1026@bk.ru>
2024-10-31 14:21:56 +0500

    init

---
+++ users/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
+
+NULLABLE = {'blank': True, 'null': True}
+
+phone_validation = RegexValidator(
+    regex=r'^(?:\+7|8)\d{10}$',  # Проверка для российских номеров телефона
+    message="Номер телефона должен быть в формате '+7XXXXXXXXXX' или '8XXXXXXXXXX'."
+)
+
+
+class User(AbstractUser):
+    """Кастомный пользователь."""
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+    token = models.CharField(max_length=50, **NULLABLE, verbose_name='token')
+
+    fullname = models.CharField(max_length=150, verbose_name='Ф.И.О.')
+    phone_number = models.CharField(unique=True, max_length=12, validators=[phone_validation],
+                                    verbose_name='номер телефона')
+    avatar = models.ImageField(upload_to='users/avatars', **NULLABLE, verbose_name='аватар')
+
+    def __str__(self):
+        return self.email
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        class Meta:
+            verbose_name = 'пользователь'
+            verbose_name_plural = 'пользователи'
