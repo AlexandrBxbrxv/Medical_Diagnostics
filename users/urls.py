7262d286660d53d788f84308f3c586a69ca65e7d
AlexandrBxbrxv <g1026@bk.ru>
2024-11-03 11:37:09 +0500

    + ссылка на контроллер завершения регистрации

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from users.apps import UsersConfig
 
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from users.views import UserLoginView, UserRegister
+from users.views import UserLoginView, UserRegister, email_verification
 
 app_name = UsersConfig.name
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', UserRegister.as_view(), name='register'),
+    path('email-confirm/<str:token>/', email_verification, name='email_confirm'),
 ]
