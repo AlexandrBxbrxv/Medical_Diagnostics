9e88f02a31cbb0431d2f187e36f983f82bee026b
AlexandrBxbrxv <g1026@bk.ru>
2024-11-03 11:46:51 +0500

    fix ссылки на users, добавил /

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-    path('users', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
