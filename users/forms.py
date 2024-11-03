c78046a69820d80449db15ee96e84d4b858cc746
AlexandrBxbrxv <g1026@bk.ru>
2024-11-03 10:37:58 +0500

    + форма для регистрации User

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.forms import ModelForm
 
 from main.forms import StyleFormMixin
+from users.models import User
 
 
 class UserLoginForm(StyleFormMixin, AuthenticationForm):
     pass
+
+
+class UserRegisterForm(StyleFormMixin, ModelForm):
+    class Meta:
+        model = User
+        fields = ('email', 'fullname', 'phone_number', 'password1', 'password2',)
