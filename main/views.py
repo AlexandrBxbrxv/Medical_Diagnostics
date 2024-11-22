393fcc0fa661f856fa99e59ac60b898e349f988f
AlexandrBxbrxv <g1026@bk.ru>
2024-11-22 17:09:16 +0500

    + docs

--- main/views.py
+++ main/views.py
@@ -2,4 +2,5 @@ from django.views.generic import TemplateView
 
 
 class IndexTemplateView(TemplateView):
+    """Контроллер для отображения главной страницы."""
     template_name = 'main/index.html'
