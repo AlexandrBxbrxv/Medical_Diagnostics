1e08c4129147c613b79b3003103b6d26da35e2ac
AlexandrBxbrxv <g1026@bk.ru>
2024-11-26 18:20:36 +0500

    + years_declination_filter

--- main/templatetags/custom_tags.py
+++ main/templatetags/custom_tags.py
@@ -4,8 +4,29 @@ register = template.Library()
 
 
 @register.filter()
-def media_filter(path):
+def media_filter(path: str) -> str:
     """Строит путь до изображений."""
-    if path is None or path == 'avatar_placeholder.jpg':
+    if path == '' or path == 'avatar_placeholder.jpg':
         return "/media/main/placeholders/avatar_placeholder.jpg"
     return f"/media/{path}"
+
+
+@register.filter()
+def years_declination_filter(years: int) -> str:
+    """Пишет правильное склонение, например 1 год, 2 года, 5 лет."""
+    word = ''
+    if years == 1:
+        word = 'год'
+    if 1 < years < 5:
+        word = 'года'
+    if 4 < years < 21 or years == 0:
+        word = 'лет'
+    if years > 20:
+        years_second_number = int(str(years)[1])
+        if years_second_number == 1:
+            word = 'год'
+        if 1 < years_second_number < 5:
+            word = 'года'
+        if 4 < years_second_number < 10 or years_second_number == 0:
+            word = 'лет'
+    return f'{years} {word}'
