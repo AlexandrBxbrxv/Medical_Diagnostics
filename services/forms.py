a1593034645509741cb4abb3d766319368e102a6
AlexandrBxbrxv <g1026@bk.ru>
2024-11-28 17:34:07 +0500

    + AppointmentModelForm

--- services/forms.py
+++ services/forms.py
@@ -1,11 +1,18 @@
 from django.forms import ModelForm
 
 from main.forms import StyleFormMixin
-from services.models import Doctor
+from services.models import Doctor, Appointment
 
 
 class DoctorModelForm(StyleFormMixin, ModelForm):
     """Форма для создания/редактирования модели Doctor"""
     class Meta:
         model = Doctor
+        exclude = ('owner',)
+
+
+class AppointmentModelForm(StyleFormMixin, ModelForm):
+    """Форма для создания/редактирования модели Doctor"""
+    class Meta:
+        model = Appointment
         exclude = ('owner',)
