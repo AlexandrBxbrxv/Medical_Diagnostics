915fe2ea73df1f0a209fa3ce436f75f1840b2a79
AlexandrBxbrxv <g1026@bk.ru>
2024-11-28 07:34:40 +0500

    + тесты

--- users/tests.py
+++ users/tests.py
@@ -1,7 +1,7 @@
 from django.db import connection
 from django.test import TestCase, Client
 
-from main.utils_for_tests import create_groups_for_test, create_users_for_tests
+from main.utils_for_tests import create_groups_for_test, create_users_for_tests, create_doctors_for_tests
 from services.models import Doctor
 
 
@@ -20,8 +20,11 @@ class PermissionTestCase(TestCase):
             *create_groups_for_test()
         )
 
+        self.doctor, self.others_doctor = create_doctors_for_tests(self.medical_staff, self.other_medical_staff)
+
         self.client = Client()
 
+# Тесты доступов не авторизованного пользователя ####################
     def test_doctor_create_unauthorized(self):
         """Создание врача неавторизованным пользователем."""
         data = {
@@ -47,14 +50,13 @@ class PermissionTestCase(TestCase):
             "/users/login/?next=/services/doctor/create/"
         )
 
-        self.assertTrue(not Doctor.objects.filter(pk=1).exists())
+        self.assertTrue(not Doctor.objects.filter(pk=5).exists())
 
     def test_doctor_list_unauthorized(self):
         """Просмотр списка врачей неавторизованным пользователем."""
 
         response = self.client.get(
-            '/services/doctor/list/',
-            format='json'
+            '/services/doctor/list/'
         )
 
         self.assertEqual(
@@ -62,6 +64,95 @@ class PermissionTestCase(TestCase):
             200
         )
 
+    def test_doctor_detail_unauthorized(self):
+        """Просмотр врача неавторизованным пользователем."""
+
+        response = self.client.get(
+            '/services/doctor/detail/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_doctor_update_unauthorized(self):
+        """Изменение врача неавторизованным пользователем."""
+
+        data = {
+            "fullname": "change"
+        }
+
+        response = self.client.post(
+            '/services/doctor/update/1/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+    def test_doctor_delete_unauthorized(self):
+        """Удаление врача неавторизованным пользователем."""
+
+        response = self.client.delete(
+            '/services/doctor/delete/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+# Тесты доступа авторизованного мед персонала к чужим объектам ######
+    def test_doctor_detail_others_object(self):
+        """Просмотр врача другого пользователя."""
+        self.client.force_login(user=self.medical_staff)
+
+        response = self.client.get(
+            '/services/doctor/detail/2/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+    def test_doctor_update_others_object(self):
+        """Изменение врача другого пользователя."""
+        self.client.force_login(user=self.medical_staff)
+
+        data = {
+            "fullname": "change"
+        }
+
+        response = self.client.put(
+            '/services/doctor/update/2/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+    def test_doctor_delete_others_object(self):
+        """Изменение врача другого пользователя."""
+        self.client.force_login(user=self.medical_staff)
+
+        response = self.client.delete(
+            '/services/doctor/delete/2/',
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+# Тесты доступов авторизованного пользователя #######################
     def test_doctor_create_by_authorized_user(self):
         """Создание врача авторизованным пользователем."""
         self.client.force_login(user=self.user)
@@ -84,5 +175,62 @@ class PermissionTestCase(TestCase):
             403
         )
 
-        self.assertTrue(not Doctor.objects.filter(pk=1).exists())
+        self.assertTrue(not Doctor.objects.filter(pk=5).exists())
 
+    def test_doctor_list_by_authorized_user(self):
+        """Просмотр списка врачей авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        response = self.client.get(
+            '/services/doctor/list/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_doctor_detail_by_authorized_user(self):
+        """Просмотр врача авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        response = self.client.get(
+            '/services/doctor/detail/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_doctor_update_by_authorized_user(self):
+        """Изменение врача авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        data = {
+            "fullname": "change"
+        }
+
+        response = self.client.put(
+            '/services/doctor/update/1/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+    def test_doctor_delete_by_authorized_user(self):
+        """Удаление врача авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        response = self.client.delete(
+            '/services/doctor/delete/1/',
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
