e59eb2335fba5fd7963f47c098a0309ac27c26ed
AlexandrBxbrxv <g1026@bk.ru>
2024-12-08 15:10:07 +0500

    + тесты

--- services/tests.py
+++ services/tests.py
@@ -1,8 +1,9 @@
 from django.db import connection
 from django.test import TestCase, Client
 
-from main.utils_for_tests import create_groups_for_test, create_users_for_tests, create_doctors_for_tests
-from services.models import Doctor
+from main.utils_for_tests import create_groups_for_test, create_users_for_tests, create_doctors_for_tests, \
+    create_appointments_for_tests
+from services.models import Doctor, Appointment
 
 
 class DoctorTestCase(TestCase):
@@ -23,10 +24,10 @@ class DoctorTestCase(TestCase):
         self.doctor, self.others_doctor = create_doctors_for_tests(self.medical_staff, self.other_medical_staff)
 
         self.client = Client()
+        self.client.force_login(user=self.medical_staff)
 
     def test_doctor_create(self):
         """Создание врача авторизованным пользователем."""
-        self.client.force_login(user=self.medical_staff)
 
         data = {
             "fullname": "Фамилия Имя Отчество",
@@ -55,7 +56,6 @@ class DoctorTestCase(TestCase):
 
     def test_doctor_list(self):
         """Просмотр списка врачей авторизованным пользователем."""
-        self.client.force_login(user=self.medical_staff)
 
         response = self.client.get(
             '/services/doctor/list/',
@@ -68,7 +68,6 @@ class DoctorTestCase(TestCase):
 
     def test_doctor_detail(self):
         """Просмотр врача авторизованным пользователем."""
-        self.client.force_login(user=self.medical_staff)
 
         response = self.client.get(
             '/services/doctor/detail/1/',
@@ -81,7 +80,6 @@ class DoctorTestCase(TestCase):
 
     def test_doctor_update(self):
         """Изменение врача авторизованным пользователем."""
-        self.client.force_login(user=self.medical_staff)
 
         data = {
             "fullname": "change"
@@ -100,11 +98,9 @@ class DoctorTestCase(TestCase):
 
     def test_doctor_delete(self):
         """Удаление врача авторизованным пользователем."""
-        self.client.force_login(user=self.medical_staff)
 
         response = self.client.delete(
             '/services/doctor/delete/1/'
-
         )
 
         self.assertEqual(
@@ -118,3 +114,118 @@ class DoctorTestCase(TestCase):
         )
 
         self.assertTrue(not Doctor.objects.filter(pk=1).exists())
+
+
+class AppointmentTestCase(TestCase):
+    """Тестирование работоспособности контроллеров модели Appointment."""
+
+    def reset_sequence(self):
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE users_user_id_seq RESTART WITH 1;")
+            cursor.execute("ALTER SEQUENCE services_appointment_id_seq RESTART WITH 1;")
+
+    def setUp(self):
+        self.reset_sequence()
+
+        self.user, self.other_user, self.medical_staff, self.other_medical_staff = create_users_for_tests(
+            *create_groups_for_test()
+        )
+
+        self.appointment, self.others_appointment = create_appointments_for_tests(
+            self.medical_staff, self.other_medical_staff
+        )
+
+        self.client = Client()
+        self.client.force_login(user=self.medical_staff)
+
+    def test_appointment_create(self):
+        """Создание приема авторизованным пользователем."""
+
+        data = {
+            "title": "test",
+            "treatment_room": 1
+        }
+
+        response = self.client.post(
+            '/services/appointment/create/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+        self.assertEqual(
+            response.url,
+            "/services/appointment/create/"
+        )
+
+        self.assertTrue(Appointment.objects.filter(pk=3))
+
+    def test_appointment_list(self):
+        """Просмотр списка приемов авторизованным пользователем."""
+
+        response = self.client.get(
+            '/services/appointment/list/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_appointment_detail(self):
+        """Просмотр приема авторизованным пользователем."""
+
+        response = self.client.get(
+            '/services/appointment/detail/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_appointment_update(self):
+        """Изменение приема авторизованным пользователем."""
+
+        data = {
+            "title": "change"
+        }
+
+        response = self.client.put(
+            '/services/appointment/update/1/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+        self.assertEqual(
+            response.url,
+            "/services/appointment/detail/1/"
+        )
+
+    def test_appointment_delete(self):
+        """Удаление приема авторизованным пользователем."""
+
+        response = self.client.delete(
+            '/services/appointment/delete/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+        self.assertEqual(
+            response.url,
+            "/services/appointment/list/"
+        )
+
+        self.assertTrue(not Appointment.objects.filter(pk=1))
