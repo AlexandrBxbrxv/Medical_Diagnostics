de5854a5d7f9f2cba16b78ba4725d644e78d57fc
AlexandrBxbrxv <g1026@bk.ru>
2024-12-08 15:56:00 +0500

    + AnalysisModelForm

--- services/forms.py
+++ services/forms.py
@@ -1,18 +1,26 @@
 from django.forms import ModelForm
 
 from main.forms import StyleFormMixin
-from services.models import Doctor, Appointment
+from services.models import Doctor, Appointment, Analysis
 
 
 class DoctorModelForm(StyleFormMixin, ModelForm):
-    """Форма для создания/редактирования модели Doctor"""
+    """Форма для создания/редактирования объекта модели Doctor."""
     class Meta:
         model = Doctor
         exclude = ('owner',)
 
 
 class AppointmentModelForm(StyleFormMixin, ModelForm):
-    """Форма для создания/редактирования модели Doctor"""
+    """Форма для создания/редактирования объекта модели Doctor."""
     class Meta:
         model = Appointment
+        exclude = ('owner',)
+
+
+class AnalysisModelForm(StyleFormMixin, ModelForm):
+    """Форма для создания/редактирования объекта модели Analysis."""
+
+    class Meta:
+        model = Analysis
         exclude = ('owner',)
