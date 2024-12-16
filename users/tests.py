b47af0761c169efd49a434c4a54d21968e7d9588
AlexandrBxbrxv <g1026@bk.ru>
2024-12-16 16:22:20 +0500

    + тесты

--- users/tests.py
+++ users/tests.py
@@ -2,8 +2,8 @@ from django.db import connection
 from django.test import TestCase, Client
 
 from main.utils_for_tests import create_groups_for_test, create_users_for_tests, create_doctors_for_tests, \
-    create_appointments_for_tests
-from services.models import Doctor, Appointment
+    create_appointments_for_tests, create_analysis_for_tests
+from services.models import Doctor, Appointment, Analysis
 
 
 class PermissionsForDoctorTestCase(TestCase):
@@ -458,6 +458,238 @@ class PermissionsForAppointmentTestCase(TestCase):
 
         response = self.client.delete(
             '/services/appointment/delete/1/',
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+
+class PermissionsForAnalysisTestCase(TestCase):
+    """Тестирование доступов к контроллерам модели Analysis."""
+
+    def reset_sequence(self):
+        with connection.cursor() as cursor:
+            cursor.execute("ALTER SEQUENCE users_user_id_seq RESTART WITH 1;")
+            cursor.execute("ALTER SEQUENCE services_analysis_id_seq RESTART WITH 1;")
+
+    def setUp(self):
+        self.reset_sequence()
+
+        self.user, self.other_user, self.medical_staff, self.other_medical_staff = create_users_for_tests(
+            *create_groups_for_test()
+        )
+
+        self.analysis, self.others_analysis = create_analysis_for_tests(
+            self.medical_staff, self.other_medical_staff
+        )
+
+        self.client = Client()
+
+# Тесты доступов не авторизованного пользователя ####################
+    def test_analysis_create_unauthorized(self):
+        """Создание анализа неавторизованным пользователем."""
+        data = {
+            "title": "test",
+            "description": "test",
+            "preparation": "test",
+            "treatment_room": 1,
+            "price": 1
+        }
+
+        response = self.client.post(
+            '/services/analysis/create/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+        self.assertEqual(
+            response.url,
+            "/users/login/?next=/services/analysis/create/"
+        )
+
+        self.assertTrue(not Analysis.objects.filter(pk=3).exists())
+
+    def test_analysis_list_unauthorized(self):
+        """Просмотр списка анализов неавторизованным пользователем."""
+
+        response = self.client.get(
+            '/services/analysis/list/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_analysis_detail_unauthorized(self):
+        """Просмотр анализа неавторизованным пользователем."""
+
+        response = self.client.get(
+            '/services/analysis/detail/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_analysis_update_unauthorized(self):
+        """Изменение анализа неавторизованным пользователем."""
+
+        data = {
+            "title": "change"
+        }
+
+        response = self.client.post(
+            '/services/analysis/update/1/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+    def test_analysis_delete_unauthorized(self):
+        """Удаление анализа неавторизованным пользователем."""
+
+        response = self.client.delete(
+            '/services/analysis/delete/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            302
+        )
+
+# Тесты доступа авторизованного мед персонала к чужим объектам ######
+    def test_analysis_detail_others_object(self):
+        """Просмотр анализа другого пользователя."""
+        self.client.force_login(user=self.medical_staff)
+
+        response = self.client.get(
+            '/services/analysis/detail/2/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+    def test_analysis_update_others_object(self):
+        """Изменение анализа другого пользователя."""
+        self.client.force_login(user=self.medical_staff)
+
+        data = {
+            "title": "change"
+        }
+
+        response = self.client.put(
+            '/services/analysis/update/2/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+    def test_analysis_delete_others_object(self):
+        """Изменение анализа другого пользователя."""
+        self.client.force_login(user=self.medical_staff)
+
+        response = self.client.delete(
+            '/services/analysis/delete/2/',
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+# Тесты доступов авторизованного пользователя #######################
+    def test_analysis_create_by_authorized_user(self):
+        """Создание анализа авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        data = {
+            "title": "test",
+            "treatment_room": 1
+        }
+
+        response = self.client.post(
+            '/services/analysis/create/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+        self.assertTrue(not Analysis.objects.filter(pk=3).exists())
+
+    def test_analysis_list_by_authorized_user(self):
+        """Просмотр списка анализов авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        response = self.client.get(
+            '/services/analysis/list/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_analysis_detail_by_authorized_user(self):
+        """Просмотр анализа авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        response = self.client.get(
+            '/services/analysis/detail/1/'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_analysis_update_by_authorized_user(self):
+        """Изменение анализа авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        data = {
+            "title": "change"
+        }
+
+        response = self.client.put(
+            '/services/analysis/update/1/',
+            data=data,
+            format='json'
+        )
+
+        self.assertEqual(
+            response.status_code,
+            403
+        )
+
+    def test_analysis_delete_by_authorized_user(self):
+        """Удаление анализа авторизованным пользователем."""
+        self.client.force_login(user=self.user)
+
+        response = self.client.delete(
+            '/services/analysis/delete/1/',
         )
 
         self.assertEqual(
