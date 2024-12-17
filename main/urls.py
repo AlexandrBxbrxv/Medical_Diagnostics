c94bae1e6a9aa6533b0951af59ffe49ad52132e3
AlexandrBxbrxv <g1026@bk.ru>
2024-12-17 20:39:53 +0500

    + ссылка на about

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import IndexTemplateView
+from main.views import IndexTemplateView, AboutTemplateView
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', IndexTemplateView.as_view(), name='index'),
+    path('about', AboutTemplateView.as_view(), name='about'),
 ]
