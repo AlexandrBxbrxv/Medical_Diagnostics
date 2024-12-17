1dfd8c11d3524ccc007d5a9d1bb167e1b41eb4ed
AlexandrBxbrxv <g1026@bk.ru>
2024-12-17 20:40:06 +0500

    + AboutTemplateView

--- main/views.py
+++ main/views.py
@@ -1,6 +1,24 @@
 from django.views.generic import TemplateView
 
+from services.models import Doctor
+
 
 class IndexTemplateView(TemplateView):
     """Контроллер для отображения главной страницы."""
     template_name = 'main/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = 'Главная'
+        return context_data
+
+
+class AboutTemplateView(TemplateView):
+    """Контроллер для отображения страницы о компании."""
+    template_name = 'main/about.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = 'О компании'
+        context_data['doctor_count'] = Doctor.objects.count()
+        return context_data
