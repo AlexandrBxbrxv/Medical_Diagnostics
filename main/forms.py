dfacbf42ef7a538dc0b2175328307ecf286296d6
AlexandrBxbrxv <g1026@bk.ru>
2024-12-19 18:32:37 +0500

    + FeedbackModelForm

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,6 @@
-from django.forms import BooleanField
+from django.forms import BooleanField, ModelForm
+
+from main.models import Feedback
 
 
 class StyleFormMixin:
@@ -9,3 +11,11 @@ class StyleFormMixin:
                 field.widget.attrs['class'] = 'form-check-input'
             else:
                 field.widget.attrs['class'] = 'form-control'
+
+
+class FeedbackModelForm(StyleFormMixin, ModelForm):
+    """Форма для создания/редактирования объекта модели Feedback."""
+
+    class Meta:
+        model = Feedback
+        exclude = ('owner', 'date_created', 'is_closed')
