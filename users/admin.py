f055aa415b2150a1d82347de1686624ee3fd32cc
AlexandrBxbrxv <g1026@bk.ru>
2024-12-23 18:24:18 +0500

    + action ban_users

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,28 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 
 from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email',)
-    search_fields = ('full_name',)
+    list_display = ('id', 'email', 'fullname', 'is_active')
+    search_fields = ('fullname', 'email',)
+    ordering = ('id', 'fullname',)
+    list_filter = ('is_active',)
+
+    actions = ['ban_users']
+
+    @admin.action(description='Ban selected users')
+    def ban_users(self, request, queryset):
+        updated = queryset.update(is_active=False)
+        self.message_user(
+            request,
+            ngettext(
+                "%d user banned",
+                "%d users banned",
+                updated,
+            )
+            % updated,
+            messages.SUCCESS,
+        )
