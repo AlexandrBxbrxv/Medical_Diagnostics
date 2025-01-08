f94e737ac5cf4e80305ea9321c73439d35b266a8
AlexandrBxbrxv <g1026@bk.ru>
2025-01-08 21:24:51 +0500

    added input

--- users/management/commands/create_admin.py
+++ users/management/commands/create_admin.py
@@ -1,26 +1,20 @@
-import os
-from django.contrib.auth.models import Group
-from dotenv import load_dotenv
-from config.settings import BASE_DIR
-
 from django.core.management import BaseCommand
 from users.models import User
 
 
-load_dotenv(BASE_DIR / '.env')
-
-
 class Command(BaseCommand):
     """Создает суперпользователя."""
+
     def handle(self, *args, **options):
+        email = input("Email: ")
+        password = input("Пароль: ")
+
         user = User.objects.create(
-            email=os.getenv('ADMIN_EMAIL'),
+            email=email,
             is_staff=True,
             is_superuser=True
         )
-        user.set_password(os.getenv('ADMIN_PASSWORD'))
 
-        user_group = Group.objects.get(name='admin')
-        user_group.user_set.add(user)
+        user.set_password(password)
 
         user.save()
