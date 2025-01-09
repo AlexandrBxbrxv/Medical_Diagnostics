eefee237b795c535ad108cb6932e0dfbf3010037
AlexandrBxbrxv <g1026@bk.ru>
2025-01-09 07:26:59 +0500

    fix

--- main/management/commands/fill_db.py
+++ main/management/commands/fill_db.py
@@ -1,3 +1,5 @@
+import json
+
 from django.core.management import BaseCommand, call_command
 
 
@@ -6,11 +8,33 @@ class Command(BaseCommand):
     help = "Заполняет базу данных из набора фикстур."
 
     def handle(self, *args, **kwargs):
+
+        users_data_file = "fixtures/users_data.json"
+        users_carts_file = "fixtures/users_carts.json"
+
+        filtrated_users_data = []
+        users_carts = []
+
+        with open(users_data_file, mode='r', encoding='utf-8') as file:
+            users_data = json.load(file)
+            for item in users_data:
+                if item["model"] == "users.cart":
+                    users_carts.append(item)
+                else:
+                    filtrated_users_data.append(item)
+
+        with open(users_carts_file, mode='w', encoding='utf-8') as file:
+            json.dump(users_carts, file, indent=2, ensure_ascii=False)
+
+        with open(users_data_file, mode='w', encoding='utf-8') as file:
+            json.dump(filtrated_users_data, file, indent=2, ensure_ascii=False)
+
         fixtures = [
             "fixtures/auth_data.json",
-            "fixtures/users_data.json",
-            "fixtures/main_data.json",
-            "fixtures/services_data.json"
+            users_data_file,
+            "fixtures/services_data.json",
+            users_carts_file,
+            "fixtures/main_data.json"
         ]
 
         for fixture in fixtures:
